63b566aab1f47786727a62e699a42bd31f8396ee
ShahzadKhichi <shahzadkhichi996@gmail.com>
2025-10-28 16:42:30 +0500

    intialized my project.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+
+// set template engine
+// app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.listen(3000, () => console.log("Listening on Port : 3000"));
