98b41dc26247b7ad78405cf323ac714a76e26f52
ShahzadKhichi <shahzadkhichi996@gmail.com>
2025-10-30 11:00:41 +0500

    cart and auth pages ared added

--- server.js
+++ server.js
@@ -6,12 +6,27 @@ const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 
 // set template engine
-// app.use(expressLayout);
+app.use(expressLayout);
 
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+//routes
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("auth/register");
+});
+
 app.listen(3000, () => console.log("Listening on Port : 3000"));
